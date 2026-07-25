d5e294561978bef8b0a115eea6e41453fe1fd9bd
kishan28p <kishankumarp228@gmail.com>
2026-07-25 18:07:00 +0530

    Fix CORS for deployed frontend

--- rideon_project/rideon_project/settings.py
+++ rideon_project/rideon_project/settings.py
@@ -138,6 +138,6 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
